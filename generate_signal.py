2f3c36bc4f38a5c54994797a49421fa897ada50f
Jeeva45680 <jeevj175@gmail.com>
2026-02-09 23:01:11 +0530

    Update generate_signal.py

--- generate_signal.py
+++ generate_signal.py
@@ -1,5 +1,8 @@
 import json
 from datetime import datetime
+import random
+
+signal = random.choice(["BUY", "SELL", "WAIT"])
 
 data = {
     "signals": [
@@ -8,8 +11,8 @@ data = {
             "time": datetime.now().strftime("%H:%M:%S"),
             "market": "FOREX",
             "pair": "EUR/USD",
-            "signal": "WAIT",
-            "confidence": 0,
+            "signal": signal,
+            "confidence": random.randint(60,90),
             "wins": 0,
             "losses": 0
         }
@@ -18,3 +21,5 @@ data = {
 
 with open("signal.json", "w") as f:
     json.dump(data, f, indent=2)
+
+print("Signal updated")
